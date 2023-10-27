9fd22cedf22b6a1081b0435da6a14445de02b03f
Pipmor <139385966+Pipmor@users.noreply.github.com>
2023-10-27 14:20:56 +0600

    t

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setUser } from "./pages/action";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const payload = {
+      name: e.target.name.value,
+      age: e.target.age.value,
+      gender: e.target.gender.value,
+    };
+    dispatch(setUser(payload));
+  };
+
+  useEffect(() => {
+    const { name, age, gender } = user;
+    console.log(`Имя: ${name}\nВозраст: ${age}\nПол: ${gender}`);
+  }, [user]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Имя" name="name" />
+          <input type="number" placeholder="Возраст" name="age" />
+          <select name="gender">
+            <option value="male">Мужчина</option>
+            <option value="female">Женщина</option>
+          </select>
+          <button>Отправить</button>
+        </form>
+      </div>
   );
-}
+};
 
 export default App;
